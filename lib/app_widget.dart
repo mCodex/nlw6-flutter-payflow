59d716ac210490aa236e66a0c938318cc5e1d294
Mateus Andrade <mat.andrade@live.com>
2021-06-22 20:47:37 -0300

    Added firebase config on android/ios

---
+++ lib/app_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'modules/home/home_page.dart';
+import 'modules/login/login_page.dart';
+import 'modules/splash/splash_page.dart';
+import 'shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PayFlow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
