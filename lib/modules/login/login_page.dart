d034f5db92bf0e95b3f73ba38baf18cad8e38fe8
Mateus Andrade <mat.andrade@live.com>
2021-06-22 21:06:22 -0300

    Added GoogleSignin function example

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -55,8 +56,19 @@ class _LoginPageState extends State<LoginPage> {
                 Padding(
                   padding: const EdgeInsets.only(left: 40, right: 40, top: 40),
                   child: SocialLoginButton(
-                    onTap: () {
-                      print("Clicked");
+                    onTap: () async {
+                      GoogleSignIn _googleSignIn = GoogleSignIn(
+                        scopes: [
+                          'email',
+                        ],
+                      );
+
+                      try {
+                        final response = await _googleSignIn.signIn();
+                        print(response);
+                      } catch (error) {
+                        print(error);
+                      }
                     },
                   ),
                 )
